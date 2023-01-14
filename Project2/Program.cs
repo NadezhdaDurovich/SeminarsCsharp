085c73984750859a9734c2876625539416dfd8b8
Nadezhda Durovich <effnylife@gmail.com>
2023-01-14 23:28:55 +0500

    проект 2_нахождение min и max из двух чисел

---
+++ Project2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// Например
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write("Max = ");
+    Console.WriteLine (a); 
+    Console.Write ("Min = "); 
+    Console.WriteLine (b); 
+} 
+else
+{
+    Console.Write("Max = ");
+    Console.WriteLine (b); 
+    Console.Write ("Min = "); 
+    Console.WriteLine (a); 
+}
