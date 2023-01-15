2c01f5b15965b693b187ecbf7aa8739088b1a1e6
Nadezhda Durovich <effnylife@gmail.com>
2023-01-15 16:26:57 +0500

    проект 10_сравнение чисел_каскадные условия

---
+++ Project10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Каскадные условия (else if $$, ||)
+
+int a=5;
+int b=25;
+if (a>10&&b>20)
+{
+    Console.WriteLine(1);
+}
+else
+{
+    Console.WriteLine(2);
+}
+// 
+// if (a>10)
+// {
+//     Console.WriteLine(1);
+// else if (a<30)
+// {
+//     Console.WriteLine(2);
+// }
+// else
+// {
+//     Console.WriteLine(3);
+// }
