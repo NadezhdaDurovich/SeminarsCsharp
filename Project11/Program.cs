985e3f23235f7780986776a20a19985fc78a4c87
Nadezhda Durovich <effnylife@gmail.com>
2023-01-15 16:58:05 +0500

    проект 11_циклические условия

---
+++ Project11/Program.cs
@@ -0,0 +1,22 @@
+﻿// Цикл (доработать - есть нерешенные ошибки!)
+int a=5;
+for (int i=0, i<10, i++)
+{
+    if (a==8||a==7)
+    {
+        continue;
+        Console.WriteLine("111");
+    }
+    Console.WriteLine(i);
+}
+
+// while (a<=10)
+// {
+//     Console.WriteLine(a);
+//     a++;
+//     if (a==9)
+//     {
+//         break;
+//     }
+// }
+// Console.WriteLine("ОК");
