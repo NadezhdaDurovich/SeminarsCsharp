885a91b17536f9b598c484186783a4b11a8e51c7
Nadezhda Durovich <effnylife@gmail.com>
2023-01-18 23:39:36 +0500

    проект 11_примеры циклов (условный while и счетный for)

--- Project11/Program.cs
+++ Project11/Program.cs
@@ -1,14 +1,27 @@
-﻿// Цикл (доработать - есть нерешенные ошибки!)
+﻿// Цикл (Условный while и счётный for)
 int a=5;
-for (int i=0, i<10, i++)
+
+for (int i=0; i<10; i++)
 {
-    if (a==8||a==7)
+    if (i==8||i==7)
     {
         continue;
         Console.WriteLine("111");
     }
     Console.WriteLine(i);
 }
+Console.WriteLine("OK");
+
+// while (a<10)
+// {
+//     a++;
+//     if (a==8)
+//     {
+//        continue; 
+//     }
+//     Console.WriteLine(a);
+// }
+// Console.WriteLine("ОК");
 
 // while (a<=10)
 // {
