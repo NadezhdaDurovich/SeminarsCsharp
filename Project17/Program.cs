c1b93601054cf2e4daaf25ed53e75d0b21c7bfa4
Nadezhda Durovich <effnylife@gmail.com>
2023-01-29 13:54:34 +0500

    проект 17_выводим на экран 3-ю цифру слева

--- Project17/Program.cs
+++ Project17/Program.cs
@@ -3,22 +3,46 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
-
+int y=10;
+int ThirdDigit (int x)
+{
+int result=-1;
+   if (x>99)
+   {
+      while(x>999)
+      {
+         x=x/y;
+      }
+      result=x%y;
+   }
+return result;
+}
 Console.Write("Введите любое число: ");
 int N=Convert.ToInt32(Console.ReadLine());
-int count=100;
 int x=N;
-int y=10;
-if ((N/count)==0)
+if (ThirdDigit(N)==-1)
 {
    Console.WriteLine($"В числе {N} третьей цифры нет");  
 }
 else
 {
-    while ((x/y)!=0)
-    {
-       x=x-x%y;
-       y=y*10;
-    }
-    Console.WriteLine($"В числе {N} третья цифра слева - это {(N%(y/100)-((N%(y/100))%(y/1000)))/(y/1000)}"); 
+   Console.WriteLine($"В числе {N} третья цифра слева - это {ThirdDigit(N)}");
 }
+// Console.Write("Введите любое число: ");
+// int N=Convert.ToInt32(Console.ReadLine());
+// int x=N;
+// int y=10;
+// int count=100;
+// if ((N/count)==0)
+// {
+//    Console.WriteLine($"В числе {N} третьей цифры нет");  
+// }
+// else
+// {
+//     while ((x/y)!=0)
+//     {
+//        x=x-x%y;
+//        y=y*10;
+//     }
+//     Console.WriteLine($"В числе {N} третья цифра слева - это {(N%(y/100)-((N%(y/100))%(y/1000)))/(y/1000)}"); 
+// }
